57fec93f246ecd59a2895720207c898f598b0b20
Daniel Kolar <77999053+donkeykong909@users.noreply.github.com>
2023-05-04 16:06:21 -0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -8,6 +8,13 @@
     //create db instance
     $mysqli = new mysqli($host, $username, $password, $database_in_use) or die ("Unable to connect");
 
+    // // cloud service?
+    //$host = "final.cg8jzr3llpk0.us-east-2.rds.amazonaws.com";
+    //$username = "root";
+    //$password = "admin123"; //changed from "usbw" for my machine
+    //$database_in_use = "final"; //changed from "final project" for my machine
+
+
     //test the connection
     //echo "Connection successful";
 ?>
